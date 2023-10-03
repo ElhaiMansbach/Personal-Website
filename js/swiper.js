85ac88fe9109460d004154b73d5066999520ae55
Elhai Mansbach <elhaimn@gmail.com>
2023-10-04 00:11:46 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+// swiper testimonial
+
+const testimonialSwiper = new Swiper(".testimonial__wrapper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  effect: "coverflow",
+  grabCursor: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+
